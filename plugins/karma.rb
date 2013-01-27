e76d569c2ee834668e006ebef402c786c1be5b36
Akshay Gupta <kitallis@gmail.com>
2013-01-28 01:30:34 +0530

    adding methods to retrieve and update karma

--- plugins/karma.rb
+++ plugins/karma.rb
@@ -1,6 +1,26 @@
 module PieMan
   class Karma < Sequel::Model
     one_to_one :user
+
+    class << self
+      def get(name)
+        user = User.where(:name => name).first
+        user.nil? ? 0 : user.karma.value
+      end
+
+      def increment(name)
+        update(name, 1)
+      end
+
+      def decrement(name)
+        update(name, -1)
+      end
+
+      def update(name, value)
+        karma = User.freate(name).karma
+        karma.update(:value => (karma.value + value))
+      end
+    end
   end
 
   Karma.dataset = DB[:karma]
